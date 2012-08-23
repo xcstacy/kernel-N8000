bfcb806c4822e1cc50a1b354546fa9cb5d8cc506
Andrew Dodd <atd7@cornell.edu>
2012-08-22 23:23:03 -0400

    sdhci: Remove Samsung's broken hack
    To avoid a divide-by-zero, Samsung returns a 0 if
    host->clock is not set.
    
    This is bad - the timeout will (of course) now be
    as short as possible.
    
    Mainline does it differently - It uses a base
    timeout value regardless of whether host->clock
    exists, then adds that value if it exists.
    
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -642,16 +642,14 @@ static u8 sdhci_calc_timeout(struct sdhci_host *host, struct mmc_command *cmd)
 	if (!data && !cmd->cmd_timeout_ms)
 		return 0xE;
 
+
 	/* timeout in us */
 	if (!data)
 		target_timeout = cmd->cmd_timeout_ms * 1000;
 	else {
-		/* patch added for divide by zero once issue. */
-		if (host && host->clock)
-			target_timeout = data->timeout_ns / 1000 +
-				data->timeout_clks / host->clock;
-		else
-			return 0;
+		target_timeout = data->timeout_ns / 1000;
+		if (host->clock)
+			target_timeout += data->timeout_clks / host->clock;
 	}
 
 	if (host->quirks & SDHCI_QUIRK_DATA_TIMEOUT_USES_SDCLK)
