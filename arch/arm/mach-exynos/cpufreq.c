1e712a31ee98de376c119cd69caf9ee794afda23
xcstacy <zkchong2005@hotmail.com>
2013-04-16 17:10:20 +0100

    cpufreq: fix freq 1704 stucked (thanks GM)

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -64,7 +64,9 @@ int exynos_verify_speed(struct cpufreq_policy *policy)
 
 unsigned int exynos_getspeed(unsigned int cpu)
 {
-	return clk_get_rate(exynos_info->cpu_clk) / 1000;
+	unsigned int ret = clk_get_rate(exynos_info->cpu_clk) / 1000;
+	if(ret == 1704000) ret = 1700000;
+	return ret;
 }
 
 static unsigned int exynos_get_safe_armvolt(unsigned int old_index, unsigned int new_index)
