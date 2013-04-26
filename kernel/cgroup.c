3055e66d615d6d0ff7e4b5ecd50c530eb1ed657d
xcstacy <zkchong2005@hotmail.com>
2013-04-26 16:01:51 +0100

    cgroup: fix exit() vs rmdir() race

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -366,10 +366,13 @@ static void free_css_set_work(struct work_struct *work)
 		struct cgroup *cgrp = link->cgrp;
 		list_del(&link->cg_link_list);
 		list_del(&link->cgrp_link_list);
+		rcu_read_lock();
 		if (atomic_dec_and_test(&cgrp->count)) {
 			check_for_release(cgrp);
 			cgroup_wakeup_rmdir_waiter(cgrp);
 		}
+		rcu_read_unlock();
+
 		kfree(link);
 	}
 	write_unlock(&css_set_lock);
